04b59194b635cd9a0def3d7c8c138c33bf820cf7
Ryland <rylandporter@gmail.com>
2026-03-19 20:19:07 -0700

    Footer.jsx: - modified format to match prototype, finished footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,29 +2,31 @@ import {FaMapMarkerAlt, FaEnvelope, FaPhoneAlt, FaInstagram} from "react-icons/f
 
 function Footer() {
   return (
-    <footer className="bg-[#C5AE98] py-15 px-8">
-      <div className="flex items-center justify-context flex-wrap gap-4">
-        <h2 className=" text-black text-3xl font-semibold">Contact Us: </h2>
+    <footer className="bg-[#C5AE98] py-5 px-8">
+      <div className="flex items-start justify-start flex-wrap gap-4 h-20">
+        <h2 className=" text-black text-5xl font-semibold">Contact Us: </h2>
+      </div>
 
+      <div className="flex items-center justify-end flex-wrap gap-4 font-semibold">
         <div className="flex items-center gap-8 flex-wrap text-black">
-          <div className="flex items-center gap-2">
-            <FaMapMarkerAlt className="text-2xl" />
-            <span className="text-sm">Sacramento, CA</span>
+          <div className="flex items-center gap-2 ">
+            <FaMapMarkerAlt className="text-3xl" />
+            <span className="text-lg">Sacramento, CA</span>
           </div>
 
           <div className="flex items-center gap-2">
-            <FaEnvelope className="text-2xl" />
-            <span className="text-sm">kinganita25@gmail.com</span>
+            <FaEnvelope className="text-3xl" />
+            <span className="text-lg">kinganita25@gmail.com</span>
           </div>
 
           <div className="flex items-center gap-2">
-            <FaPhoneAlt className="text-2xl" />
-            <span className="text-sm">916.640.6007</span>
+            <FaPhoneAlt className="text-3xl" />
+            <span className="text-lg">916.640.6007</span>
           </div>
 
           <div className="flex items-center gap-2">
-            <FaInstagram className="text-2xl" />
-            <span className="text-sm">@kingssoap</span>
+            <FaInstagram className="text-3xl" />
+            <span className="text-lg">@kingssoap</span>
           </div>
           
 

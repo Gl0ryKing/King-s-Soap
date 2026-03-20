51a269dc75237f667e82de91d555a5ef7486b010
Ryland <rylandporter@gmail.com>
2026-03-19 18:43:01 -0700

    Footer.jsx: - added client phone number - enlarged footer to better match prototype

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import {FaMapMarkerAlt, FaEnvelope, FaPhoneAlt, FaInstagram} from "react-icons/f
 
 function Footer() {
   return (
-    <footer className="bg-[#C5AE98] py-4 px-8">
+    <footer className="bg-[#C5AE98] py-15 px-8">
       <div className="flex items-center justify-context flex-wrap gap-4">
         <h2 className=" text-black text-3xl font-semibold">Contact Us: </h2>
 
@@ -11,12 +11,16 @@ function Footer() {
             <FaMapMarkerAlt className="text-2xl" />
             <span className="text-sm">Sacramento, CA</span>
           </div>
-          
+
           <div className="flex items-center gap-2">
             <FaEnvelope className="text-2xl" />
             <span className="text-sm">kinganita25@gmail.com</span>
           </div>
 
+          <div className="flex items-center gap-2">
+            <FaPhoneAlt className="text-2xl" />
+            <span className="text-sm">916.640.6007</span>
+          </div>
           
 
         </div>

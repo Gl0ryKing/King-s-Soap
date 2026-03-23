e8dfbacbc68cfad26d4195fa51f639bf73c60005
imranahmad@csus.edu <imranahmad@csus.edu>
2026-03-22 20:10:22 -0700

    DT-241 Readded after was removed by a different commit.

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -31,7 +31,9 @@ const Home = () => {
 
   return (
   <div style={{ width: "100%", height: "100%"  }}>
-  <h1 className="text-4xl text-[#FFFFFF] font-[Inria_Serif] pl-11 pt-8"> Home </h1>
+  {/*<h1 className="text-4xl text-[#FFFFFF] font-[Inria_Serif] pl-11 pt-8"> Home </h1>*/}
+
+  <HomeImage /> {/*Load HomePageImage, includes text over image*/}
 
  {/*Carousel for popular items*/}
  

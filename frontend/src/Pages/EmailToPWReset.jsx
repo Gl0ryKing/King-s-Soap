beebaecc56759c8fc1310d88c72007f2c2c66178
Ryland <rylandporter@gmail.com>
2026-04-03 17:36:06 -0700

    EmailToPWReset.jsx: - created Email prompt - created email text entry prompt - created buttons

--- frontend/src/Pages/EmailToPWReset.jsx
+++ frontend/src/Pages/EmailToPWReset.jsx
@@ -10,28 +10,41 @@ function EmailToPWReset() {
         className="absolute h-full w-full object-cover z-0"
       />
 
-      {/*Account Verification Notifier*/}
+      {/*Email Entry to initiate Password Reset*/}
       <div className="z-20 w-full max-w-md rounded-2xl bg-white px-6 py-8 shadow-lg -mt-40">
         <div className="space-y-6">
 
-          <div className="text-center">
-            <h1 className="text-2xl font-medium text-gray-800">
-              Verify your account
-            </h1>
-            <p className="mt-2 text-base text-gray-600">
-              Please check your email for instructions on verifying your account.
-            </p>
-          </div>
+          <form class="space-y-6">
+                <div>
+                    <label class="block text-2xl font-medium text-gray-800 mb-2">
+                    Email
+                    </label>
+                    <input
+                    type="email"
+                    placeholder="Value"
+                    class="w-full h-12 px-4 rounded-xl border border-gray-300 text-gray-700 placeholder-gray-400 outline-none focus:ring-2 focus:ring-gray-400"
+                    />
+                </div>
 
 
             {/* Change button type to this when we end up handling button */}
             {/* <button type="button" onClick={handleResendEmail}> */}
-          <button
-            type="button"
-            className="w-full h-12 rounded-xl bg-zinc-800 text-xl font-medium text-white cursor-pointer"
-          >
-            Resend Email
-          </button>
+          <div className="flex gap-4">
+            <button
+              type="button"
+              className="flex-1 h-12 rounded-xl border border-zinc-800 text-xl font-medium text-zinc-800 cursor-pointer bg-transparent"
+            >
+              Cancel
+            </button>
+
+            <button
+              type="button"
+              className="flex-1 h-12 rounded-xl bg-zinc-800 text-xl font-medium text-white cursor-pointer"
+            >
+              Reset Password
+            </button>
+          </div>
+          </form>
         </div>
       </div>
     </div>
